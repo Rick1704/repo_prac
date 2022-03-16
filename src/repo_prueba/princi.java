09026e2af8415a5c129ed5470f8ac9f34692028a
Ricardo <ricardosf00@educastur.es>
2022-03-16 19:37:51 +0100

    commit de pro

--- src/repo_prueba/princi.java
+++ src/repo_prueba/princi.java
@@ -1,9 +1,20 @@
 package repo_prueba;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class princi {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+		String edad;
+		int cadena;
+
 		System.out.println("1º modificación");
+		edad = bufer.readLine();
+		cadena = Integer.parseInt(edad);
+		System.out.println(cadena);
 
 	}
 
